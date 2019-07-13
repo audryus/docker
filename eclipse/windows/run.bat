40f891554902645e9c34653b29c2b50febf4cde1
Audryus <audryus@users.noreply.github.com>
2019-07-13 17:16:21 -0300

    Change the way the bat runs (Power shell only)

--- eclipse/windows/run.bat
+++ eclipse/windows/run.bat
@@ -1,4 +1,3 @@
 set /p local_ip="This machine IP (Eg.:192.168.1.104): "
-set-variable -name DISPLAY -value %local_ip%:0.0
 set /p dir="Where Eclipse files will be? (Eg.: c:\eclipse-docker): "
-docker run -ti --rm -e DISPLAY=$DISPLAY -v %dir%:/home/developer audryus/jee-eclipse:12.2019.06
+docker run -ti --rm -e DISPLAY=%local_ip%:0.0 -v %dir%:/home/developer audryus/jee-eclipse:12.2019.06
